14101da0ba9a3c477656032cd1258820fba9cc71
Saiprasad Shettar <45258145+shydrate@users.noreply.github.com>
2020-01-31 14:16:01 +0530

    Update test_demo1.py

--- test_demo1.py
+++ test_demo1.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import time
+import pytest
+
 def test_search():
     print("Running selenium test")
     try:
@@ -14,4 +16,15 @@ def test_search():
         driver.close()
         driver.quit()
     except NoSuchElementException as NEA:
-        print("Element not present {}".format(NEA))
+        print("Element not present :{}".format(NEA))
+
+@pytest.mark.smoke
+def test_shydrate():
+    try:
+        print("Selenium test for shydrate")
+        driver = webdriver.Chrome(executable_path="D:\\Downloaded\\chromedriver.exe")
+        driver.get("http://shydrate.com/")
+        time.sleep(2)
+        print("Selenium test for shydrate over")
+    except NoSuchElementException as NA:
+        print("Element not present :{}".format(NA))
